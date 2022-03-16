6d5dae49c1e6f94135519c1c4560ab16fee1923f
Ertholin <ertholinulysse@gmail.com>
2022-03-16 13:25:01 -0400

    Spécifier dans le package utils (Constants) les différents Nomsdes constantes pour chaque opération dans le Controller (Api) -> Controller CommandeFournisseur

--- src/main/java/com/ertho/gestiondestosck/controller/api/CommandeFournisseurApi.java
+++ src/main/java/com/ertho/gestiondestosck/controller/api/CommandeFournisseurApi.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.ertho.gestiondestosck.utils.Constants.APP_ROOT;
+import static com.ertho.gestiondestosck.utils.Constants.*;
 
-@Tag(name = APP_ROOT + "/commandeFournisseurs")
+@Tag(name = COMMANDE_FOURNISSEUR_ENDPOINT)
 public interface CommandeFournisseurApi {
 
-    @PostMapping(value = APP_ROOT + "/commandeFournisseurs/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = CREATE_COMMANDE_FOURNISSEUR_ENDPOINT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Enregistrer une commande fournisseur", description = "Cette méthode permet d'enregisrer ou modifier une commande fournisseur")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "L'objet commande fournisseur créé / modifié"),
@@ -23,7 +23,7 @@ public interface CommandeFournisseurApi {
     })
     CommandeFournisseurDto save(@RequestBody CommandeFournisseurDto dto);
 
-    @GetMapping(value = APP_ROOT + "/commandeFournisseurs/{idCommandeFournisseur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = FIND_COMMANDE_FOURNISSEUR_BY_ID_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Rechercher une commande fournisseur par ID", description = "Cette méthode permet de rechercher une commande fournisseur par son ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "La commande fournisseur a été trouvée dans la base de données"),
@@ -31,7 +31,7 @@ public interface CommandeFournisseurApi {
     })
     CommandeFournisseurDto findById(@PathVariable("idCommandeFournisseur") Integer id);
 
-    @GetMapping(value = APP_ROOT + "/commandeFournisseurs/{codeCommandeFournisseur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = FIND_COMMANDE_FOURNISSEUR_BY_CODE_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Rechercher une commande fournisseur par code", description = "Cette méthode permet de rechercher une commande fournisseur par son CODE")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "La commande fournisseur a été trouvée dans la base de données"),
@@ -39,7 +39,7 @@ public interface CommandeFournisseurApi {
     })
     CommandeFournisseurDto findByCode(@PathVariable("codeCommandeFournisseur") String code);
 
-    @GetMapping(value = APP_ROOT + "/commandeFournisseurs/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = FIND_ALL_COMMANDE_FOURNISSEUR_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Renvoie la liste des commandes fournisseur", description = "Cette méthode permet de rechercher et de renvoyer la liste des commandes fournisseur qui existe " +
             "dans la Base de données")
     @ApiResponses(value = {
@@ -47,7 +47,7 @@ public interface CommandeFournisseurApi {
     })
     List<CommandeFournisseurDto> findAll();
 
-    @DeleteMapping(value = APP_ROOT + "/commandeFournisseur/delete/{idCommandeFournisseur}")
+    @DeleteMapping(value = DELETE_COMMANDE_FOURNISSEUR_ENDPOINT)
     @Operation(summary = "Supprimer une commande fournisseur", description = "Cette méthode permet de supprimer une commande fournisseur par ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "La commande fournisseur a été supprimée dans la base de données")
--- src/main/java/com/ertho/gestiondestosck/utils/Constants.java
+++ src/main/java/com/ertho/gestiondestosck/utils/Constants.java
@@ -3,4 +3,16 @@ package com.ertho.gestiondestosck.utils;
 public interface Constants {
 
     String APP_ROOT = "gestiondestock/v1";
+
+    String COMMANDE_FOURNISSEUR_ENDPOINT = APP_ROOT + "/commandesfournisseurs";
+
+    String CREATE_COMMANDE_FOURNISSEUR_ENDPOINT = COMMANDE_FOURNISSEUR_ENDPOINT + "/CREATE";
+
+    String FIND_COMMANDE_FOURNISSEUR_BY_ID_ENDPOINT = COMMANDE_FOURNISSEUR_ENDPOINT + "/{idCommandeFournisseur}";
+
+    String FIND_COMMANDE_FOURNISSEUR_BY_CODE_ENDPOINT = COMMANDE_FOURNISSEUR_ENDPOINT + "/{codeCommandeFournisseur}";
+
+    String FIND_ALL_COMMANDE_FOURNISSEUR_ENDPOINT = COMMANDE_FOURNISSEUR_ENDPOINT + "/all";
+
+    String DELETE_COMMANDE_FOURNISSEUR_ENDPOINT = COMMANDE_FOURNISSEUR_ENDPOINT + "/delete/{idCommandeFournisseur}";
 }
